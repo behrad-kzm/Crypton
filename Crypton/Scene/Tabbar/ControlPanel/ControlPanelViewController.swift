6d545183ae31dd9a737f864491297f74f6aec106
behrad kazemi <behradkzm@gmail.com>
2019-07-22 18:49:30 +0430

    complete UI Control Panel

--- Crypton/Scene/Tabbar/ControlPanel/ControlPanelViewController.swift
+++ Crypton/Scene/Tabbar/ControlPanel/ControlPanelViewController.swift
@@ -10,8 +10,53 @@ import UIKit
 import RxSwift
 class ControlPanelViewController: UIViewController {
 	
+	//Title
 	@IBOutlet weak var symbolLabel: UILabel!
+	
+	//CurrentPrice
+	@IBOutlet weak var arrowDownImage: UIImageView!
+	@IBOutlet weak var arrowUpImage: UIImageView!
 	@IBOutlet weak var currentPrice: UILabel!
+	@IBOutlet weak var currentPriceDollarSignLabel: UILabel!
+	
+	//CurrentProfit
+	@IBOutlet weak var currentProfitTitleLabel: UILabel!
+	@IBOutlet weak var currentProfitPercentLabel: UILabel!
+	
+	//Table
+	@IBOutlet weak var sideTitleLabel: UILabel!
+	@IBOutlet weak var sideLabel: UILabel!
+	
+	@IBOutlet weak var quantityTitleLabel: UILabel!
+	@IBOutlet weak var quantityLabel: UILabel!
+	
+	@IBOutlet weak var totalProfitTitleLabel: UILabel!
+	@IBOutlet weak var totalProfitLabel: UILabel!
+	
+	@IBOutlet weak var changeBTCTitleLabel: UILabel!
+	@IBOutlet weak var changeBTCLabel: UILabel!
+	
+	@IBOutlet weak var changeUSDTitleLabel: UILabel!
+	@IBOutlet weak var changeUSDLabel: UILabel!
+	
+	@IBOutlet weak var currentBTCTitleLabel: UILabel!
+	@IBOutlet weak var currentBTCLabel: UILabel!
+	
+	@IBOutlet weak var currentUSDTitleLabel: UILabel!
+	@IBOutlet weak var currentUSDLabel: UILabel!
+	
+	@IBOutlet weak var stopLossTitleLabel: UILabel!
+	@IBOutlet weak var stopLossLabel: UILabel!
+	
+	@IBOutlet weak var leverageTitleLabel: UILabel!
+	@IBOutlet weak var leverageLabel: UILabel!
+	
+	@IBOutlet weak var autoUpdateTitleLabel: UILabel!
+	@IBOutlet weak var autoUpdateSwitch: UISwitch!
+	
+	@IBOutlet weak var autoReverseTitleLabel: UILabel!
+	@IBOutlet weak var autoReverseSwitch: UISwitch!
+	
 	@IBOutlet weak var endPositionButton: UIButton!
 	var viewModel: ControlPanelViewModel!
   let disposeBag = DisposeBag()
